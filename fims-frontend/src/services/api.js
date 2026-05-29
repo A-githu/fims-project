19313c4e9e66f9c0bbc00a81f754951a89510c79
AzizKoungoue <azizkoungoue@email.com>
2026-05-29 13:10:51 +0100

    hardcode api url https

--- fims-frontend/src/services/api.js
+++ fims-frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import toast from 'react-hot-toast'
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://fims-project-production.up.railway.app'
+const API_URL = 'https://fims-project-production.up.railway.app'
+
 const api = axios.create({
     baseURL: API_URL,
     headers: {
